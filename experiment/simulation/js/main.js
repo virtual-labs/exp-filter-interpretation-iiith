561f0cd66cd38e5be7abf129c888bae2871a0010
MostlyK <135974627+MostlyKIGuess@users.noreply.github.com>
2025-01-30 15:20:37 +0530

    filter magnitude fix

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -454,7 +454,7 @@ function syst(){
     // Normalize the original signal values for plotting
     var maxSigValue = Math.max(...originalSigValues.map(Math.abs));
     var normalizedSigValues = originalSigValues.map(value => value / maxSigValue);
-    var normalizedSpectrumValue = ampSpecOut.map(value => value / maxSigValue);
+    var normalizedSpectrumValue = ampSpecOut.map(value => value / maxAmpSpec);
 
 
 
